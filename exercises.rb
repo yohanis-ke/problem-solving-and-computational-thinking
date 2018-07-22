712bc70a7d5a08f0a1e6b31ce656268dd0b3c7f8
yohanis-ke <yohanes.ke@gmail.com>
2018-07-22 14:12:26 -0400

    I tried the the assisdgnment on problem solving and computetional thinking

---
+++ exercises.rb
@@ -0,0 +1,14 @@
+transport = [
+  {train: "72C", frequency_in_minutes: 15, direction: "north"},
+  {train: "72D", frequency_in_minutes: 15, direction: "south"},
+  {train: "610", frequency_in_minutes: 5, direction: "north"},
+  {train: "611", frequency_in_minutes: 5, direction: "south"},
+  {train: "80A", frequency_in_minutes: 30, direction: "east"},
+  {train: "80B", frequency_in_minutes: 30, direction: "west"},
+  {train: "110", frequency_in_minutes: 15, direction: "north"},
+  {train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+var= transport[:direction{|i| i["train"]} == "111"]
+  puts var
+ # cities.each do |key, val|
+# cities [:Newyork]
---
+++ research-and-echnical-documentation.rb
@@ -0,0 +1,52 @@
+# String
+#  length
+#    a method
+#    accept the string/array
+#    return the length of string/array
+#    arr=[1,2,3]
+#    arr.length
+#    =>3
+#  strip
+#   is a method
+#   accept the string
+#   return the string after removing white space
+#
+#   "  hello ".strip
+#   =>"hello"
+# split
+#   is a method
+#   accept the string and pattern of argument
+#   return the splited array based on the pattern argument
+#
+#   " hello".split('')
+#   [" ", "n", "o", "w", "'", "s", " ", " ", "t", "h", "e", " ", "t", "i", "m", "e"]
+# Array
+#  first
+#   is a method that accept an array and returns the first element of array
+#   arr=['a','b','c']
+#   arr.first =>> 'a'
+#  delete_at
+#   is a method that accept the index of element and delete the element of array in the specific index
+#   arr.delete_at[2]
+#
+#  delete
+#   is a method that accept the value argument and delete it self from the array, if it's nor exists return nil
+#   arr.delete('a')
+# Hash
+#  to_a
+#   converts a hash to nested array.
+#   hsh={"a"=>3,"b"=>4,"c"=>5}
+#   hsa.to_a
+#  has_key?
+#  has_value?
+#
+# Time
+#  now
+#   return the current time
+# File
+#  exist?
+#
+#
+#  extname
+#
+#  is a method returns the file extension
